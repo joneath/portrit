dbc2e21547108b3e929a7f69cdbbf1729f186c6d
Jerry Lin <infinixd@gmail.com>
2011-01-28 22:07:01 +0000

    Changed /bin/sh to /bin/bash in nginx script.

--- nx_restart.sh
+++ nx_restart.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if ps ax | grep -v grep | grep "nginx:" < /dev/null
+if ps ax | grep -v grep | grep "nginx: " < /dev/null
 then
 	echo "Nginx service is running, everything is fine"
 else
