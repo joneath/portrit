9f2d1f428655bf937d27ed4584d9463f7dcffcd9
Jerry Lin <infinixd@gmail.com>
2011-01-28 21:54:02 +0000

    Updated grep argument for nginx to detect.

--- nx_restart.sh
+++ nx_restart.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-
-if ps ax | grep -v grep | grep "nginx" < /dev/null
+if ps ax | grep -v grep | grep "nginx:" < /dev/null
 then
 	echo "Nginx service is running, everything is fine"
 else
 	/etc/init.d/nginx start 
 	mail -s " Portrit Nginx Failed" jerrylin86@gmail.com portritinc@gmail.com < /var/www/email/nginx_message.txt
 fi
-
