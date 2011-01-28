d8a69bff575785eea59cbb58d3624653de4af1db
Jerry Lin <infinixd@gmail.com>
2011-01-28 07:36:56 +0000

    Fixed issues on apache and nginx scripts, had to add another -v on grep.

--- httpd.sh
+++ httpd.sh
@@ -2,10 +2,10 @@
 
 SERVICE='httpd'
 
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
+if ps ax | grep -v grep | grep -v "httpd.sh" | grep $SERVICE > /dev/null
 then
 	echo "Apache service is running, everything is fine"
 else
 	/etc/init.d/httpd start 
-	mail -s " Portrit Apache Failed" jerrylin86@gmail.com portritinc@gmail.com < /var/www/email/httpd_message.txt
+	mail -s "Portrit Apache Failed" jerrylin86@gmail.com portritinc@gmail.com < /var/www/email/nginx_message.txt
 fi
--- nginx.sh
+++ nginx.sh
@@ -2,10 +2,11 @@
 
 SERVICE='nginx'
 
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
+if ps ax | grep -v grep | grep -v "nginx.sh" | grep $SERVICE < /dev/null
 then
 	echo "Nginx service is running, everything is fine"
 else
 	/etc/init.d/nginx start 
 	mail -s " Portrit Nginx Failed" jerrylin86@gmail.com portritinc@gmail.com < /var/www/email/nginx_message.txt
 fi
+
