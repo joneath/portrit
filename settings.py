258e6b5000fa3479d5065f2073c1dc1bca997185
Jonathan Eatherly <jonathan.eatherly@gmail.com>
2011-04-26 06:40:23 +0000

    Changes -  WebApp 	- Removed MySQL conf from settings.py

--- settings.py
+++ settings.py
@@ -68,11 +68,11 @@ if ENV == "LOCAL":
         print "Could not connect to Mongo"
     
 elif ENV == "TEST":
-    DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    DATABASE_NAME = 'portrit'             # Or path to database file if using sqlite3.
-    DATABASE_USER = 'root'             # Not used with sqlite3.
+    DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+    DATABASE_USER = ''             # Not used with sqlite3.
     DATABASE_PASSWORD = ''         # Not used with sqlite3.
-    DATABASE_HOST = '10.212.93.12'             # Set to empty string for localhost. Not used with sqlite3.
+    DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
     DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
     
     BASE_URL = "http://test.portrit.com"
