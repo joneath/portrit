9c732bb9ce21ab62f8a058237f98a2ba82527620
Jonathan Eatherly <jonathan.eatherly@gmail.com>
2011-05-11 22:40:10 +0000

    Changes:  - Web App 	* Fixed wsgi conf.

--- portrit_wsgi.py
+++ portrit_wsgi.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path = ['/var/www/portrit'] + sys.path
+sys.path = ['/var/www/portrit', '/var/www'] + sys.path
 from django.core.handlers.wsgi import WSGIHandler
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'portrit.settings'
 application = WSGIHandler()
